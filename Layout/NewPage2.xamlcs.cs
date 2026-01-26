817086eebdd500407ecd4b110da2ab31b1d225c1
celnik-213 <uT_4000@zsp3zamosc.pl>
2026-01-26 10:57:38 +0100

    Add files via upload

---
+++ Layout/NewPage2.xamlcs.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+
+namespace Layout;
+
+public partial class NewPage2 : ContentPage
+{
+	public NewPage2()
+	{
+		InitializeComponent();
+	}
+    private async void OnThumbnailTapped(object sender, EventArgs e)
+	{
+		var thumbnai = sender as Image;
+
+		if (thumbnai != null)
+		{
+			await DisplayAlert(
+				"Miniatura",
+				"Klikniêto w miniaturê w galerii",
+				"OK");
+		}
+	}
+}
